793cdd1bc0fd0581136bf76f61d5c2e997647a96
Michelle McGhee <michmac202@gmail.com>
2022-08-03 10:04:39 -0700

    zoom and pan closes #1

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,7 +1,74 @@
 <script>
+	import { select, zoom } from "d3";
+	import viewport from "$stores/viewport";
+	import { onMount } from "svelte";
+	import loadImage from "$utils/loadImage.js";
+
 	export let src;
 	export let zoomable;
+	export let wrapper;
+
+	let z;
+	let background;
+	let ratio = 0;
+
+	const handleZoom = (e) => {
+		select(background).style(
+			"transform",
+			`translate(${e.transform.x}px, ${e.transform.y}px) scale(${e.transform.k})`
+		);
+	};
+
+	$: background, $viewport.width, setupZoom();
+
+	const setupZoom = () => {
+		const zoomableW = $viewport.width > 1024 ? $viewport.width : 1024;
+		const zoomableH = ratio * zoomableW;
+
+		console.log({ zoomableW, zoomableH });
+
+		z = zoom()
+			.scaleExtent([1, 4])
+			.translateExtent([
+				[0, 0],
+				[zoomableW, zoomableH]
+			])
+			.on("zoom", handleZoom);
+
+		select(wrapper).call(z);
+	};
+
+	onMount(async () => {
+		const img = await loadImage("assets/img/city.jpg");
+		ratio = img.height / img.width;
+
+		if (zoomable) {
+			setupZoom();
+		}
+	});
 </script>
 
+<img {src} bind:this={background} class="background" />
+
 <style>
+	img.background {
+		transform-origin: 0px 0px;
+		min-height: 100%;
+		min-width: 1024px;
+
+		width: 100%;
+		height: auto;
+
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
+
+	@media screen and (max-width: 1024px) {
+		/* TODO: center it on mobile? */
+		img.background {
+			/* left: 50%;
+			margin-left: -512px; */
+		}
+	}
 </style>
--- src/components/Scroll.svelte
+++ src/components/Scroll.svelte
@@ -1,20 +1,14 @@
 <script>
-	import { select, zoom } from "d3";
 	import Sound from "$components/Sound.svelte";
 	import Vendor from "$components/Vendor.svelte";
+	import Background from "$components/Background.svelte";
 	import Scrolly from "$components/helpers/Scrolly.svelte";
-	import viewport from "$stores/viewport";
 	import { language } from "$stores/misc.js";
-	import { onMount, tick } from "svelte";
 
 	export let steps;
 
-	let z;
 	let scrollValue;
 	let wrapper;
-	let background;
-	let width = 0;
-	let height = 0;
 
 	$: currentStep = scrollValue !== undefined ? steps[scrollValue] : undefined;
 	$: currentBackground =
@@ -30,59 +24,13 @@
 			? `assets/img/vendor.jpeg`
 			: undefined;
 	let zoomable = true;
-	// $: zoomable = scrollValue > 6 && currentStep.background === "city";
-
-	const handleZoom = (e) => {
-		select(background).style(
-			"transform",
-			`translate(${e.transform.x}px, ${e.transform.y}px) scale(${e.transform.k})`
-		);
-	};
-
-	$: background, $viewport.width, setDimensions();
-	const setDimensions = () => {
-		if (background) {
-			width = background.clientWidth;
-			height = background.clientHeight;
-
-			setupZoom();
-		}
-	};
-
-	$: console.log({ width, height });
-
-	const setupZoom = () => {
-		console.log("setting up", width, height);
-		z = zoom()
-			.scaleExtent([1, 4])
-			.translateExtent([
-				[0, 0],
-				[width, height]
-			])
-			.on("zoom", handleZoom);
-
-		select(wrapper).call(z);
-	};
-
-	onMount(async () => {
-		if (zoomable) {
-			await tick();
-			setDimensions();
-			// SO CLOSE! <3
-			// TODO: issue, getting a height that's too small on the first call
-		}
-	});
+	//$: zoomable = scrollValue > 6 && currentStep.background === "city";
 </script>
 
 <div class="steps">
 	<div class="sticky" bind:this={wrapper}>
 		{#if currentBackground}
-			<img src={currentBackground} bind:this={background} class="background" />
-			<!-- <div
-				class="background"
-				bind:this={background}
-				style={`--url: url(${currentBackground})`}
-			/> -->
+			<Background src={currentBackground} {zoomable} {wrapper} />
 		{/if}
 	</div>
 
@@ -124,42 +72,6 @@
 		width: 100%;
 		height: 100vh;
 	}
-	.background {
-		/* background-image: var(--url);
-		width: 100%; */
-
-		/* height: 100vh; */
-
-		/* Center and scale the image nicely */
-		/* background-position: center;
-		background-repeat: no-repeat;
-		background-size: cover;
-		transform-origin: 0px 0px; */
-	}
-	img.background {
-		transform-origin: 0px 0px;
-		/* Set rules to fill background */
-		min-height: 100%;
-		min-width: 1024px;
-
-		/* Set up proportionate scaling */
-		width: 100%;
-		height: auto;
-
-		/* Set up positioning */
-		position: fixed;
-		top: 0;
-		left: 0;
-	}
-
-	@media screen and (max-width: 1024px) {
-		/* Specific to this particular image */
-		img.background {
-			left: 50%;
-			margin-left: -512px; /* 50% */
-		}
-	}
-
 	.placeholder {
 		width: 100%;
 		height: 100vh;
