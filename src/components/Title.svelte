f1660c4641689f5f6118760399fadb648421b84f
Michelle McGhee <michmac202@gmail.com>
2022-08-17 08:51:42 -0700

    update github pages

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -8,11 +8,9 @@
 		const el = document.getElementById("scroll-to-explore");
 		el.scrollIntoView({ block: "center" });
 	};
-
-	const url = "url(/assets/img/city.jpg)";
 </script>
 
-<section id="title" style={`background-image: ${url}`}>
+<section id="title">
 	<h1>{@html hed[$language]}</h1>
 	<h2>{@html dek[$language]}</h2>
 
@@ -25,7 +23,7 @@
 
 <style>
 	section {
-		/* background-image: url(/assets/img/city.jpg); */
+		background-image: url(/assets/img/city.jpg);
 		height: 100vh;
 		background-position: center;
 		background-repeat: no-repeat;
