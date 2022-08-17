0afb9c4ac4c691314cb1aa11e81385ac544df014
Michelle McGhee <michmac202@gmail.com>
2022-08-17 14:26:45 -0700

    interactive sounds

--- src/components/InteractiveLayer.svelte
+++ src/components/InteractiveLayer.svelte
@@ -1,16 +1,43 @@
 <script>
+	import Sound from "$components/Sound.svelte";
+	import copy from "$data/copy.json";
+
+	let src;
+
+	const ids = copy.soundBank.map((d) => d.id);
+
+	const onClick = (e) => {
+		const clickedId = e.target.id.replace("-button", "");
+		src = `assets/sound/${clickedId}.mp3`;
+	};
 </script>
 
-<div class="test" />
+<div class="container">
+	{#each ids as id}
+		<button id={`${id}-button`} on:click|stopPropagation={onClick}>{id}</button>
+	{/each}
+	<button class="stop" on:click={() => (src = undefined)}>stop</button>
+</div>
+
+{#if src}
+	<Sound {src} />
+{/if}
 
 <style>
-	.test {
-		background: red;
-		opacity: 0.4;
-		height: 100px;
-		width: 100px;
+	.container {
 		position: absolute;
-		top: 50%;
-		left: 40%;
+		top: 30%;
+		left: 30%;
+		display: flex;
+		flex-wrap: wrap;
+		width: 500px;
+	}
+	button {
+		background: gold;
+		opacity: 0.7;
+		height: 50px;
+	}
+	.stop {
+		background: red;
 	}
 </style>
